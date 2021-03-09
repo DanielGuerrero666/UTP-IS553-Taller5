4306869811976ebc39a77c413c1091633ece983e
Daniel Guerrero <DanielGDevil@gmail.com>
2021-03-08 21:55:02 -0500

    Reversion

--- taller5/src/main/java/com/juandanielnoriegaguerrero/prg4/ex2/LibretaDeDirecciones.java
+++ taller5/src/main/java/com/juandanielnoriegaguerrero/prg4/ex2/LibretaDeDirecciones.java
@@ -2,11 +2,12 @@
 
 package com.juandanielnoriegaguerrero.prg4.ex2;
 
+
 import java.util.Scanner;
 import com.juandanielnoriegaguerrero.prg4.ex1.RegistroLibreta;
 
 public class LibretaDeDirecciones{
-    private RegistroLibreta[] libreta = new RegistroLibreta[100];
+    private static List<RegistroLibreta> libreta = new ArrayList<>();
     private Integer indexEntrada = 0;
 
     public RegistroLibreta[] getLibreta() {
@@ -20,8 +21,15 @@ public class LibretaDeDirecciones{
     public static void añadirEntrada(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Complete la informacion del registro de la siguiente forma,"+
-             "'Nombre' 'Dirección' 'Número de telefono' 'Correo electronico'");
+             "'Nombre', 'Dirección', 'Número de telefono', 'Correo electronico'");
         RegistroLibreta entrada = new RegistroLibreta(scan.next(), scan.next(), scan.next(), scan.next());
+        libreta.add(entrada);
+        scan.close();
+    }
+
+    public static void borrarEntrada(){
+        Scanner scan = new Scanner(System.in);
+        var index = scan.nextLine();
     }
 
 
